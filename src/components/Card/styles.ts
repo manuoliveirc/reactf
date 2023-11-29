745695858f93c5cebabbd12fb652750cf5c451dd
manuoliveirc <126250653+manuoliveirc@users.noreply.github.com>
2023-11-29 10:13:36 -0300

    Update styles.ts

--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components"
 import { colors } from "../../styles/GlobalStyle"
 
 export const Aside = styled.aside`
-  background-color: ${colors.thirdLight};
+  background-color: ${colors.primary};
   border-radius: 1rem;
   padding: 1rem;
   line-height: 2rem;
