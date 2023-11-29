bc374d8b6f08aae69598ed0e03410e45b1de7ae0
manuoliveirc <manuoliveiravga@gmail.com>
2023-11-29 10:25:28 -0300

    final

--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components"
 import { colors } from "../../styles/GlobalStyle"
 
 export const Aside = styled.aside`
-  background-color: ${colors.primary};
+  background-color: ${colors.third};
   border-radius: 1rem;
   padding: 1rem;
   line-height: 2rem;
